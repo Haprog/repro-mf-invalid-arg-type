1f2e4e8e556ed9052fb493604cd5895ada423fa0
Kari Söderholm <kari.soderholm@solita.fi>
2025-01-14 14:58:27 +0200

    Minor updates to dev mode and production build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  devServer: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+/**
+ * @typedef {import('webpack').Configuration} WebpackConfiguration
+ */
+
+/** @type {() => WebpackConfiguration} */
+module.exports = (env) => {
+  const isProd = env.production === true;
+  const isDevelopment = !isProd;
+
+  console.log(`Mode: ${isProd ? `PRODUCTION` : 'DEVELOPMENT'}\n`);
+
+  return {
+    entry: './src/index',
+    mode: isProd ? 'production' : 'development',
+    ...(isDevelopment
+      ? {
+          devServer: {
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+              'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+            },
+            static: path.join(__dirname, 'dist'),
+            port: 3002,
+          },
+        }
+      : {}),
+    output: {
+      publicPath: 'auto',
     },
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    resolve: {
+      extensions: ['.ts', '.js'],
     },
-    port: 3002,
-  },
-  output: {
-    publicPath: 'auto',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-typescript'],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            presets: ['@babel/preset-typescript'],
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'repro',
+        filename: 'remoteEntry.js',
+        dev: isDevelopment,
+        exposes: {
+          './Button': './src/Button',
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'repro',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Button': './src/Button',
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+  };
 };
